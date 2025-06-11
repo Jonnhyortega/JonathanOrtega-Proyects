9aa1fdaa9d890e4ee37a53ffe2e7262311d0a98f
Jonathan Ortega <132107501+Jonnhyortega@users.noreply.github.com>
2025-06-11 20:22:52 -0300

    update

--- Portfolio/src/utils/chatbot.js
+++ Portfolio/src/utils/chatbot.js
@@ -1,5 +1,9 @@
+const API_URL = process.env.NODE_ENV === 'development'
+  ? 'http://localhost:8000/api/chat/'
+  : 'https://ia-portfolio-fxyb.onrender.com/api/chat/';
+
 export async function sendMessage(message) {
-  const response = await fetch('http://localhost:8000/api/chat/ ' || "https://ia-portfolio-fxyb.onrender.com", {
+  const response = await fetch("https://ia-portfolio-fxyb.onrender.com/api/chat/", {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ message }),
@@ -7,5 +11,3 @@ export async function sendMessage(message) {
   const data = await response.json();
   return data.response;
 }
-
-
